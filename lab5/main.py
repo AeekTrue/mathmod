f0038f2ac8fd6e3d3a01b73e73790cdb3a95dd24
Aeek True <aeektrue@gmail.com>
2024-09-08 17:32:51 +0300

    task 5.1 done

---
+++ lab5/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def make_polynomial(a):
+    def Pm(x):
+
+
+def LSA(x, y, phi):
+    '''Least squares approximation'''
+    n = len(x)
+    a = np.zeros_like(phi)
+    s = sum((sum(a * phi(x[i])) - y [i])**2 for i in range(n))
+
+# %% Задача 5.1
+
+a, b =
